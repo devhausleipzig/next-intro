074296c672d6ec38238bd0d363f464c952f2374e
Dan McAtee <danielmcatee@me.com>
2023-02-13 11:48:58 +0100

    add counter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import Head from "next/head";
+import { useState } from "react";
 
 export default function Home() {
+  const [counter, setCounter] = useState(0);
   return (
     <>
       <Head>
         <title>Booking - Home</title>
       </Head>
       <div>Hello NextJS</div>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+      <span>{counter}</span>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
     </>
   );
 }
